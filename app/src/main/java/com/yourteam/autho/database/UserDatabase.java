9c907c898b00223238c3e8ba9bdb4703dbd8736a
Christianxer0 <christianleano434@gmail.com>
2026-08-28 18:06:35 +0800

    database and & complete User file

---
+++ app/src/main/java/com/yourteam/autho/database/UserDatabase.java
@@ -0,0 +1,19 @@
+package com.yourteam.autho.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.yourteam.autho.models.User;
+
+import java.security.MessageDigest;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+
+public class UserDatabase {
+
+}
--- app/src/main/java/com/yourteam/autho/models/User.java
+++ app/src/main/java/com/yourteam/autho/models/User.java
@@ -13,42 +13,100 @@ public class User {
     private String sessionToken;
     private boolean isAuthenticated;
 
-    private User() {}
+    private User() {
+    }
 
-    private User(String username, String email, String passwordHash, String Salt) {
-        this.username =username;
+    private User(String username, String email, String passwordHash, String Salt, String createdAt, String lastLogin, String sessionToken) {
+        this.username = username;
         this.email = email;
         this.passwordHash = passwordHash;
         this.Salt = Salt;
         this.biometricEnabled = false;
+        this.createdAt = createdAt;
+        this.lastLogin = lastLogin;
+        this.sessionToken = sessionToken;
+        this.isAuthenticated = false;
     }
     //getter and setter
 
     //Id
-    public int getId(){return id;}
-    public void setId(int id) {this.id = id;}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     //username
-    public String getUsername() {return  username;}
-    public void setUsername() {this.username = username;}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     //email
-    public String getEmail() {return email;}
-    public void setEmail(){this.email = email;}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     //passwordHash
-    public String getPasswordHash(){return passwordHash;}
-    public void setPasswordHash() {this.passwordHash = passwordHash;}
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 
     //salt
-    public String getSalt(){return  Salt;}
-    public void setSalt(){this.Salt = Salt;}
+    public String getSalt() {
+        return Salt;
+    }
+
+    public void setSalt(String Salt) {
+        this.Salt = Salt;
+    }
 
     //pinhash
-    public String getPinHash(){return pinHash;}
-    public void setPinHash(){this.pinHash = pinHash;}
+    public String getPinHash() {
+        return pinHash;
+    }
 
-    public boolean isBiometricEnabled(){return biometricEnabled;}
-    public void setBiometricEnabled(){}
+    public void setPinHash(String pinHash) {
+        this.pinHash = pinHash;
+    }
+
+    //Biometric
+    public boolean isBiometricEnabled() {
+        return biometricEnabled;
+    }
+
+    public void setBiometricEnabled(boolean biometricEnabled) {
+        this.biometricEnabled = biometricEnabled;
+    }
+
+    //Create At
+    public String getCreatedAt(){return createdAt;}
+    public void setCreatedAt(String createdAt){this.createdAt = createdAt;}
+
+    //Last Login
+    public String getLastLogin(){return lastLogin;}
+    public void setLastLogin(String lastLogin){this.lastLogin = lastLogin;}
+
+    //Session Token
+    public String getSessionToken(){return sessionToken;}
+    public void setSessionToken(String sessionToken){this.sessionToken = sessionToken;}
+
+    //Authentication
+    public boolean isAuthenticated(){return isAuthenticated;}
+    public void setAuthenticated(boolean isAuthenticated) {this.isAuthenticated = isAuthenticated;}
 
 }
+
